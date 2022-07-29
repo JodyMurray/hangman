7fcba8285d3b922d3a48908dc168aa9af0cb4c87
Jody M <jodyamm@hotmail.com>
2022-07-29 19:53:58 +0000

    Start while loop and fix indentation

--- run.py
+++ run.py
@@ -15,17 +15,23 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    #get user input
+    while len(word_letters) > 0:
 
-    elif user_letter in used_letters:
-        print('\nYou have already guessed that letter. Please try again.')
-    else:
-        print('\nInvalid character')
+
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('\nYou have already guessed that letter. Please try again.')
+    
+        else:
+            print('\nInvalid character')
 
 user_input = input('Type something:')
 print(user_input)
+hangman()
 
