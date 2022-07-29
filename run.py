6bb06262e30d09cbf6e0405d0b8b87e60502842e
Jody M <jodyamm@hotmail.com>
2022-07-29 16:24:16 +0000

    Words.py created with a string of random words and beginning of run.py structure began

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(words) 
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    user_letter = input('Guess a letter: ').upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print('\nYou have already guessed that letter. Please try again.')
+    else:
+        print('\nInvalid character')
+
+user_input = input('Type something:')
+print(user_input)
+
